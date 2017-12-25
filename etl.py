bfbfb72d4531b105aa257413e090ca20a810c02f
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2017-12-25 08:53:49 +0200

    Refactor

--- etl.py
+++ etl.py
@@ -11,20 +11,19 @@ class ETL:
 
     def max_items(self, df):
         """ Calculates the maximum number of items in one order. """
-        return df[['customer_id', 'order_id', 'num_items']]\
-            .groupby(['customer_id', 'order_id']).sum().\
-            reset_index().groupby(['customer_id']).max().\
-            drop('order_id', axis=1).\
+        order_items = df[['customer_id', 'order_id', 'num_items']]\
+            .groupby(['customer_id', 'order_id']).sum()
+        maximum_items = order_items.reset_index().groupby(['customer_id']).max()
+        return maximum_items.drop('order_id', axis=1).\
             rename(columns={'num_items': 'max items'})
 
     def max_revenue(self, df):
         """ Calculates the maximum revenue in one order. """
-        df = df[['customer_id', 'order_id', 'revenue']]\
-            .groupby(['customer_id', 'order_id']).sum()\
-            .reset_index().drop('order_id', axis=1)\
-            .groupby('customer_id').max().\
-            rename(columns={'revenue': 'max revenue'})
-        return df
+        order_revenue = df[['customer_id', 'order_id', 'revenue']]\
+            .groupby(['customer_id', 'order_id']).sum()
+        maximum_revenue = order_revenue.reset_index().drop('order_id', axis=1)\
+            .groupby('customer_id').max()
+        return maximum_revenue.rename(columns={'revenue': 'max revenue'})
 
     def total_revenue(self, df):
         """ Calculates the total revenue for a customer across all orders. """
@@ -33,26 +32,27 @@ class ETL:
 
     def total_orders(self, df):
         """ Calculates the total number of orders for a customer. """
-        return df[['customer_id', 'order_id']].groupby(['customer_id']).\
-            order_id.nunique().to_frame().\
+        unique_orders = df[['customer_id', 'order_id']].\
+            groupby(['customer_id']).order_id.nunique()
+        return unique_orders.to_frame().\
             rename(columns={'order_id': 'total orders'})
 
     def days_since_last_order(self, df):
         """ Calculates the number of days since a customer's last order. """
-        days = (df[['customer_id', 'created_at_date']].groupby('customer_id').
-                max()['created_at_date'].subtract(pd.Timestamp(
-                    datetime(2017, 10, 17))).abs() /
-                numpy.timedelta64(1, 'D')).astype(int)
-        return days.to_frame().\
-            rename(columns={'created_at_date': 'days since last order'})
+        last_order = df[['customer_id', 'created_at_date']].\
+            groupby('customer_id').max()['created_at_date']
+        interval = last_order.subtract(pd.Timestamp(datetime(2017, 10, 17))).abs()
+        interval_in_days = (interval / numpy.timedelta64(1, 'D')).astype(int)
+        return interval_in_days.to_frame().rename(columns={'created_at_date':
+                                                           'days since last order'})
 
     def calculate_clv(self):
         """ Calculates CLV for all customers """
-        customer_data = self.max_items(self.df).\
-            join(self.max_revenue(self.df)).join(
-                self.total_orders(self.df)).join(
-                    self.total_revenue(self.df)).join(
-                        self.days_since_last_order(self.df))
+        customer_data = self.max_items(self.df)
+        customer_data = customer_data.join(self.max_revenue(self.df))
+        customer_data = customer_data.join(self.total_orders(self.df))
+        customer_data = customer_data.join(self.total_revenue(self.df))
+        customer_data = customer_data.join(self.days_since_last_order(self.df))
         model = dill.load(open("model.dill", 'rb'))
         result = pd.DataFrame()
         for index, row in customer_data.iterrows():
